6cc929ac83eabcad723f6de454829d52facbe56f
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-03 14:37:59 +0300

    Customer/Order entity creation

---
+++ Core/ETicaretAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        string Name { get; set; }
+        string Surname { get; set; }
+        string UserName { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string Phone { get; set; }
+        string Address { get; set; }
+        
+    }
+}
