374375e668577596d69b95fcbb3319aab2434d31
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-05 16:00:29 +0300

    abstract irepo creation for all entity in db inside of application/repositories & concrete repo creation for same entities inside of persistence/repositories

---
+++ Core/ETicaretAPI.Application/Repositories/Product/IProductWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IProductWriteRepository : IWriteRepository<Product>//generic yapmamıza gerek yok çünkü bunun product olduğu kesin
+    {
+    }
+}
